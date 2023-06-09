1dd34a266140df777fd7a52c4c3e7f4447d04fc1
Frolov Evgeniy <frolovea2010@yandex.ru>
2023-06-09 16:34:13 +0400

    Add file .env.template. Changed config.py

--- Echo_BOT/config.py
+++ Echo_BOT/config.py
@@ -1,1 +1,12 @@
-BOT_TOKEN = 'Your token'
+import os
+from dotenv import load_dotenv
+
+
+# Find .env file with os variables
+load_dotenv(".env")
+
+# retrieve config variables
+try:
+    BOT_TOKEN = os.getenv('BOT_TOKEN')
+except (TypeError, ValueError) as ex:
+    print("Error while reading config:", ex)
